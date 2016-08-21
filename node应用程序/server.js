9b14bea8a88644b4d5748aea26db2d0bfc020bd6
xiaotian wang <xiaotian wang>
2016-08-21 22:26:13 +0800

    困了

---
+++ node应用程序/server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = require('url');
+
+function start(route,handle) {
+    http.createServer(function(request, response) {
+    	var pathname = url.parse(request.url).pathname;
+    	console.log("Request for" + pathname + "received.");
+
+    	route(handle,pathname,response);
+    	
+    }).listen(8888);
+    console.log("Server has started")
+}
+exports.start = start;
